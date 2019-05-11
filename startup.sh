4027268f950f4646015aaa668466347c5f6500f6
yaylinda <lindazheng1993@gmail.com>
2019-05-10 23:28:05 -0500

    added reference startup script for ec2

---
+++ startup.sh
@@ -0,0 +1,5 @@
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo docker build https://github.com/yaylinda/my-website.git -t my-website
+sudo docker run -d -p 4200:4200 my-website
